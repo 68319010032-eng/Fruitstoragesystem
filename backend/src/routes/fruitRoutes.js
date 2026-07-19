361ae98d8e7a85429da9772db27d68a0864eb967
68319010032-eng <68319010032@loeitech.ac.th>
2026-07-19 20:44:26 +0700

    feat: add route for getting fruits expiring soon

--- backend/src/routes/fruitRoutes.js
+++ backend/src/routes/fruitRoutes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router  = express.Router();
 const {
   getAllFruits,
+  getExpiringSoon,
   getFruitById,
   createFruit,
   updateFruit,
   deleteFruit,
 } = require('../controllers/fruitController');
 
-router.get('/',      getAllFruits);
-router.get('/:id',   getFruitById);
-router.post('/',     createFruit);
-router.put('/:id',   updateFruit);
-router.delete('/:id', deleteFruit);
+router.get('/',               getAllFruits);
+router.get('/expiring-soon',  getExpiringSoon);
+router.get('/:id',            getFruitById);
+router.post('/',              createFruit);
+router.put('/:id',            updateFruit);
+router.delete('/:id',         deleteFruit);
 
 module.exports = router;
